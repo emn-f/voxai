6464d7e0d067479571daa36926f45925c2e6064b
Emanuel Ferreira <95362058+emn-f@users.noreply.github.com>
2025-05-19 17:39:30 -0300

    Atualizar o sobre.py

--- data/sobre.py
+++ data/sobre.py
@@ -23,12 +23,12 @@ SOBRE = """
     </div>
     <div class="vox-sidebar-title" style="font-size:1.1em;">Equipe do Projeto</div>
     <ul class="vox-sidebar-equipe">
-        <li><b>Emanuel Ferreira</b> — Engenharia de Software (Diretor do Vox)</li>
-        <li><b>Alicia Batista</b> — Direito</li>
-        <li><b>Brenda Pires</b> — Direito</li>
-        <li><b>Fernanda Souza</b> — Biomedicina</li>
-        <li><b>Kauã Araujo </b> — Engenharia de Software</li>
-        <li><b>Lucca Pertigas</b> — Engenharia de Software</li>
-        <li><b>Marcio Ventura</b> — Engenharia de Software</li>
+        <li><b>Emanuel Ferreira</b> (Diretor do Vox)</li>
+        <li><b>Alicia Batista</b></li>
+        <li><b>Brenda Pires</b></li>
+        <li><b>Fernanda Souza</b></li>
+        <li><b>Kauã Araujo </b></li>
+        <li><b>Lucca Pertigas</b></li>
+        <li><b>Marcio Ventura</b></li>
     </ul>
 """

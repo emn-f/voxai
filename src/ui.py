198ad813d6a1e26125d780acdd74d4f26b7b7618
Emanuel <emnarlanferreira@gmail.com>
2025-05-22 13:53:47 -0300

    Inclusão de botão pra limpar chat

--- src/ui.py
+++ src/ui.py
@@ -3,8 +3,15 @@ import time
 
 def configurar_pagina():
     st.set_page_config(page_title='VoxAI', page_icon='🌈')
-    st.title("Vox AI")
-    st.caption("Assistente de Apoio e Informação LGBTQIA+")
+    st.markdown(
+        """
+        <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
+            <h1 style="text-align: center;">Vox AI 🌈</h1>
+            <p style="text-align: center; color: gray;">Assistente de Apoio e Informação LGBTQIA+</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 def carregar_css(path="static/style.css"):
     with open(path) as f:
@@ -12,6 +19,9 @@ def carregar_css(path="static/style.css"):
 
 def carregar_sidebar(sobre, git_version, rodape):
     with st.sidebar:
+        if st.button("🧹 Limpar chat"):
+            st.session_state.pop("hist", None)
+            st.session_state.pop("hist_exibir", None)
         st.markdown(sobre, unsafe_allow_html=True)
         st.sidebar.markdown(f"<span style='color: #88888888;'>{git_version}</span>", unsafe_allow_html=True)
         st.markdown(rodape, unsafe_allow_html=True)
@@ -19,4 +29,4 @@ def carregar_sidebar(sobre, git_version, rodape):
 def stream_resposta(resposta):
     for letra in resposta:
         yield letra
-        time.sleep(0.01)
+        time.sleep(0.009)

9d71cfad2ff06d7e23e976995fcd250f49d350ac
Emanuel <emnarlanferreira@gmail.com>
2025-05-25 14:12:43 -0300

    CSS reorganizado

--- data/prompts/ui_content.py
+++ data/prompts/ui_content.py
@@ -11,58 +11,49 @@ Como posso te ajudar agora?
 """
 
 SIDEBAR = """
-    <style>
-    .vox-sidebar-title {
-        font-size: 1.4em;
-        font-weight: bold;
-        color: transparent;
-        background: linear-gradient(90deg, #e63946, #f1c40f, #43aa8b, #4361ee, #b5179e);
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        background-clip: text;
-        margin-bottom: 0.5em;
-    }
-    .vox-sidebar-section, .vox-sidebar-equipe, .vox-sidebar-parcerias {
-        color: #fff;
-    }
-    .vox-sidebar-equipe li {
-        margin-bottom: 0.3em;
-    }
-    .vox-sidebar-parcerias a {
-        color: #e75480 !important; /* rosa */
-        font-weight: bold;
-    }
-    </style>
     <div class="vox-sidebar-title">Sobre o Vox</div>
     <div class="vox-sidebar-section">
         <b>Vox AI</b> é um assistente de apoio e informação <b>LGBTQIA+</b>. Aqui você encontra acolhimento, informações e recursos confiáveis.<br>
     </div>
     <br>
-    <div class="vox-sidebar-title">Parcerias</div>
-    <ul class="vox-sidebar-parcerias" style="list-style: none; padding-left: 0;">
-        <li>
-            <a href="https://www.instagram.com/casamariellefrancobr/" target="_blank" style="text-decoration:none;">
+    <div class="vox-sidebar-title">Links úteis</div> <ul class="vox-sidebar-links"> <li>
+            <a href="https://www.instagram.com/casamariellefrancobr/" target="_blank">
                 Casa Marielle Franco
             </a>
         </li>
+        <li>
+            <a href="https://www.instagram.com/projetovoxai/" target="_blank">
+                Insta do Vox
+            </a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/company/assistentevox/" target="_blank">
+                Vox no LinkedIn
+            </a>
+        </li>
+        <li>
+            <a href="mailto:assistentedeapoiolgbtvox@gmail.com" target="_blank" class="vox-contact-link">
+                Fale com a gente
+            </a>
+        </li>    
     </ul>
-    <div style="color:#888; font-size:0.95em; margin-top:1em;">
+    <div class="disclaimer" style="color:#888; font-size:0.95em; margin-top:1em;">
         O Vox AI está em constante desenvolvimento e pode cometer equívocos.
     </div>
 
-    <div style="text-align:center; margin-top:2em;">
-        <a href="https://github.com/emn-f/voxai" target="_blank" style="text-decoration:none;">
+    <div class="footer-links" style="text-align:center; margin-top:2em;">
+        <a href="https://github.com/emn-f/voxai" target="_blank">
             <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/github.svg" alt="GitHub" style="height:1.5em; vertical-align:middle; margin-right:5px; filter: grayscale(1);" />
         </a>
     </div>
     
-    <div style="text-align:center; margin-top:0.5em;">
-        <a href="https://github.com/emn-f/voxai/blob/master/PRIVACY_POLICY.md" target="_blank" style="color:#888; text-decoration:none; font-size:0.9em;">
+    <div class="footer-policy" style="text-align:center; margin-top:0.5em;">
+        <a href="https://github.com/emn-f/voxai/blob/master/PRIVACY_POLICY.md" target="_blank">
         Política de Privacidade
         </a>
     </div>
     
-    <div style="text-align:center; color:#888; font-size:0.95em; margin-top:0.5em;">
+    <div class="copyright" style="text-align:center; color:#888; font-size:0.95em; margin-top:0.5em;">
         Copyright © 2025 Vox AI
     </div>      
 """

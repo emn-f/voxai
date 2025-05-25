53853b0a0ffa49afaad069262df973c746dcd090
Emanuel <emnarlanferreira@gmail.com>
2025-05-25 17:37:59 -0300

    Remoção de comentários

--- startup_patch.py
+++ startup_patch.py
@@ -1,5 +1,4 @@
 import sys
 import types
 
-# Substitui torch.classes por um módulo fictício para evitar introspecção (bug irritante que fica poluindo o log)
 sys.modules['torch.classes'] = types.ModuleType('torch.classes')
